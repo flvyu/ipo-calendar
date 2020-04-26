e6c82a498399e99d18f8bca76e5a33a69070e5dd
Flavyoo <flavioandrade1918@gmail.com>
2020-04-26 01:13:34 -0400

    Updated IPO table

--- src/components/IPOTable.js
+++ src/components/IPOTable.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Box, Typography } from '@material-ui/core'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 
 const EXAMPLE_DATA = {
   date: '2020-04-03',
@@ -13,10 +14,11 @@ const EXAMPLE_DATA = {
   totalSharesValue: 158355000
 }
 export default function IPOTable({ data }) {
+  const isLargeScreen = useMediaQuery(theme => theme.breakpoints.up('lg'))
   return (
     <Box display="flex" justifyContent="center">
       {data.length === 0 ? (
-        <Box width="40%" p={4} mt={2} borderRadius={10} boxShadow={2}>
+        <Box width={isLargeScreen ? '40%' : '100%'} p={4} mt={2} borderRadius={10} boxShadow={2}>
           <Typography align="center">
             No data available for the current date range. Try selecting new dates.
           </Typography>
