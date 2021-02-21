f277f57a877a3390de5def5f93f88bca0195b395
Flavyoo <flavioandrade1918@gmail.com>
2021-02-21 01:41:34 -0500

    Added company IPO data page

--- src/constants/ui_contants.js
+++ src/constants/ui_contants.js
@@ -1,1 +1,11 @@
 export const MAX_SCREEN_WIDTH = 1180
+export const COMPANY_INFORMATION_FIELDS = [
+  { dataKey: 'name', title: 'Company' },
+  { dataKey: 'symbol', title: 'Symbol' },
+  { dataKey: 'price', title: 'Expected Price' },
+  { dataKey: 'date', title: 'Expected Date' },
+  { dataKey: 'numberOfShares', title: 'Shares' },
+  { dataKey: 'totalSharesValue', title: 'Volume' },
+  { dataKey: 'status', title: 'Status' },
+  { dataKey: 'exchange', title: 'Exchange' },
+]
--- src/pages/CompanyDataPage.js
+++ src/pages/CompanyDataPage.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { Helmet } from 'react-helmet'
 import { Typography, AppBar, Toolbar, IconButton, Box } from '@material-ui/core'
+import { formatDate, formatPrice } from '../helpers/FormatHelpers'
 import { makeStyles } from '@material-ui/core/styles'
 import { Link as RouterLink } from '@reach/router'
 import { Home as HomeIcon } from '@material-ui/icons'
+import { COMPANY_INFORMATION_FIELDS } from '../constants/ui_contants'
+import { EMPTY_STRING } from '../constants/util_constants'
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -23,20 +26,20 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: theme.typography.fontWeightBold,
     color: theme.palette.primary.dark,
   },
+  fieldTitle: {
+    color: theme.palette.primary.dark,
+    fontWeight: theme.typography.fontWeightBold,
+  },
+  row: {
+    borderBottom: '1px solid gray',
+    margin: theme.spacing(2),
+  },
 }))
 
 export default function CompanyDataPage({ location }) {
   const classes = useStyles()
-  const {
-    name,
-    symbol,
-    price,
-    date: ipoDate,
-    numberOfShares,
-    totalSharesValue,
-    status,
-    exchange,
-  } = location.state.data
+  const data = location.state.data
+  const { name, totalSharesValue } = data
   return (
     <Layout>
       <Helmet>
@@ -76,11 +79,44 @@ export default function CompanyDataPage({ location }) {
           <Typography align="center">
             In this page you will find information for{' '}
             <span className={classes.companySpan}>{name}</span> To find IPO
-            information for other companies go to the <a href="/">Home</a> page to start
-            searching.
+            information for other companies go to the <a href="/">Home</a> page
+            to start searching.
           </Typography>
         </Box>
       </Box>
+      <div>
+        {COMPANY_INFORMATION_FIELDS.map((field) => {
+          let currentData = data[field.dataKey]
+          switch (field.dataKey) {
+            case 'date':
+              currentData = formatDate(currentData)
+              break
+            case 'price':
+              currentData = formatPrice(currentData)
+              break
+            case 'numberOfShares':
+              currentData = currentData
+                ? currentData.toLocaleString()
+                : EMPTY_STRING
+              break
+            case 'totalSharesValue':
+              currentData = currentData
+                ? formatPrice(currentData.toLocaleString())
+                : EMPTY_STRING
+              break
+            default:
+          }
+
+          return (
+            <div className={classes.row}>
+              <Typography variant="h6" className={classes.fieldTitle}>
+                {field.title}
+              </Typography>
+              <Typography>{currentData}</Typography>
+            </div>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
